08ac7da1e23ebf6648c56c0086b6784fb8151567
akshay kalucha <akshaykalucha@gmail.com>
2020-05-20 00:09:57 +0530

    initial COmmit

---
+++ src/RootMain.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Gallery from './components/gallery/Gallery';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+
+export class RootMain extends Component {
+    render() {
+        return (
+            <Gallery />
+        )
+    }
+}
+
+export default RootMain
